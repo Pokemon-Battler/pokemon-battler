e22de5d4fc89e47f7c4f1ecefaae40101f4a36d5
Quentin McKay <quentinmckay1@gmail.com>
2023-05-20 20:55:53 +1000

    feat: display grid of poke images + cleanup css

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import { useEffect } from 'react';
-import './App.css';
-import { useGetPokemon } from './hooks/useGetPokemon';
-import { useGlobalPokemonData } from './context/globalPokemonList';
-import { useStorage } from './hooks/useStorage';
+import { useEffect } from 'react'
+import './App.css'
+import { useGetPokemon } from './hooks/useGetPokemon'
+import { useGlobalPokemonData } from './context/globalPokemonList'
+import { useStorage } from './hooks/useStorage'
 
 function App() {
-  const { getPokemon, isLoading, error } = useGetPokemon()
-  const { pokemonList, setPokemonList } = useGlobalPokemonData()
-  const { setPersistenPokemonList } = useStorage()
+    const { getPokemon, isLoading, error } = useGetPokemon()
+    const { pokemonList, setPokemonList } = useGlobalPokemonData()
+    const { setPersistenPokemonList } = useStorage()
 
-  // Just checking if the API call worked
-  useEffect(() => {
-    getPokemon()
-    setPersistenPokemonList(pokemonList)
-  }, [])
+    // Just checking if the API call worked
+    useEffect(() => {
+        getPokemon()
+        setPersistenPokemonList(pokemonList)
 
+        console.log(pokemonList[0])
+    }, [])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className='text-7xl'>Pokemon Battler</h1>
-        
-      </header>
+    return (
+        <div>
+            <header>
+                <h1 className='text-7xl text-center'>Pokemon Battler</h1>
+            </header>
 
-      {pokemonList && pokemonList.map((pokemon, index) => {
-        return (
-          <div key={index}>
-            <p>{pokemon.name}</p>
-          </div>
-        )
-      })}
+            <main>
 
-    </div>
-  );
+				
+                {/* Todo: turn this into PokemonPickerList component */}
+                <div className='grid grid-cols-4 gap-2'>
+                    {pokemonList &&
+                        pokemonList.map((pokemon, index) => (
+                            <div className='flex items-center justify-center rounded bg-red-500/10 hover:bg-red-500/20'>
+                                <p>{pokemon.name}</p>
+                                <img
+                                    src={pokemon.sprites.front_default}
+                                    alt='pokemon selection image'
+                                />
+                            </div>
+                        ))}
+                </div>
+            </main>
+        </div>
+    )
 }
 
-export default App;
+export default App
