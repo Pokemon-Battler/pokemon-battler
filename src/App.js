2ad73091cf72cd17ffc3a4811941a0d6de4b209f
Quentin McKay <quentinmckay1@gmail.com>
2023-05-20 20:20:54 +1000

    removed default app.js stuff

--- src/App.js
+++ src/App.js
@@ -19,19 +19,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='text-7xl'>Test</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='text-7xl'>Pokemon Battler</h1>
+        
       </header>
 
       {pokemonList && pokemonList.map((pokemon, index) => {
