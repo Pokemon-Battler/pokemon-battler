837334d1dc7db5fcfa7539eab8258f8675b93ff4
socialtonics <mjkgarrow@gmail.com>
2023-05-20 16:37:28 +1000

    feat: created global context for pokemon fighters

--- src/context/globalFighterData.js
+++ src/context/globalFighterData.js
@@ -1,26 +1,47 @@
-import { createContext, useContext, useEffect, useReducer, useState } from "react"
-import { useLocalStorage } from "react-use"
+import { createContext, useContext, useReducer } from "react"
 
+// Initial fighter data to load into reducer
+const initialFighterData = {
+    fighter1: {
+        pokemonId: 1
+    },
+    fighter2: {
+        pokemonId: 3
+    }
+}
 
+// Fighter reducer
+const pokemonReducer = (prevState, action) => {
+    switch (action.type) {
+        case 'setup':
+            console.log('Setup fighter')
+            return prevState
 
-export const PokemonFighters = createContext(null)
+        case 'update':
+            console.log('Updating fighter')
+            return prevState
 
+        default:
+            return prevState
+    }
+}
 
-export function useGlobalPokemonData() {
+// Variable to hold Fighter context
+const PokemonFighters = createContext(null)
+
+// Custom hook to use the global Fighter context
+export function useGlobalFighterData() {
     return useContext(PokemonFighters)
 }
 
 
 // Component to provide global context to app
-export default function PokemonProvider(props) {
-    // Initialise reducer state and dispatcher 
-    // const [globalNotesData, globalNotesDispatcher] = useReducer(pokemonReducer, initialData)
-
-    const [fighterData, setFightData] = useState([])
-
+export default function FighterDataProvider(props) {
+    // Initialise reducer state and dispatcher
+    const [fighterData, fighterDispatch] = useReducer(pokemonReducer, initialFighterData)
 
     return (
-        <PokemonFighters.Provider value={{ fighterData, setFightData }}>
+        <PokemonFighters.Provider value={{ fighterData, fighterDispatch }}>
             {props.children}
         </PokemonFighters.Provider>
     )
