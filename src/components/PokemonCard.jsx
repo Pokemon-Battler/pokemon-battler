54956b6a67edbb5f5a01e686045a836757eb0769
Quentin McKay <quentinmckay1@gmail.com>
2023-05-21 00:03:31 +1000

    updated PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,26 @@
+import { useEffect } from 'react'
+
 import { useGlobalPokemonData } from '../context/globalPokemonList'
 
-const PokemonCard = ({ fighterNum, pokemonId }) => {
+import { capitalize } from '../utils/helperFunctions'
 
-	const { pokemonList, setPokemonList } = useGlobalPokemonData()
-	
-    return <div className="border">
-		<h2 className="text-center">Fighter {fighterNum}</h2>
-		{pokemonList[pokemonId]}
-	</div>
+const PokemonCard = ({ fighterNum, pokemonId }) => {
+    const { pokemonList, setPokemonList } = useGlobalPokemonData()
+
+    const fighter = pokemonList[pokemonId] || null
+
+    return (
+        <div className='border rounded bg-emerald-200/10'>
+            {fighter && (
+                <div className="flex flex-col items-center ">
+                    <h2>Fighter {fighterNum}</h2>
+                    <p className='text-4xl font-bold'>
+                        {capitalize(fighter.name)}
+                    </p>
+                    <img src={fighter.sprites.front_default} alt='pokemon fighter' />
+                </div>
+            )}
+        </div>
+    )
 }
 export default PokemonCard
