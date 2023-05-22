8229afd6ede9ec7448b32b91b751fd9067b081b2
socialtonics <mjkgarrow@gmail.com>
2023-05-22 11:20:19 +1000

    feat: pokemon are saved to local storage to speed up reload

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -2,7 +2,7 @@ import { useLocalStorage } from "react-use"
 
 // This hook allows for all the storage to be read, set and deleted from one place
 export function useStorage() {
-    const [persistenPokemonList, setPersistenPokemonList, removePersistenPokemonList] = useLocalStorage('pokemonList', null);
+    const [persistenPokemonList, setPersistenPokemonList, removePersistenPokemonList] = useLocalStorage('pokemonList', []);
     const [persistenPokemonFighter, setPersistenPokemonFighter, removePersistenPokemonFighter] = useLocalStorage('pokemonFighters', []);
 
     return { persistenPokemonList, setPersistenPokemonList, removePersistenPokemonList, persistenPokemonFighter, setPersistenPokemonFighter, removePersistenPokemonFighter }
