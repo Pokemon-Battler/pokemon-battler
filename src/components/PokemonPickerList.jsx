6f272814f8caa01f1c24da64a3251a96ed6e7c81
socialtonics <mjkgarrow@gmail.com>
2023-05-22 12:33:17 +1000

    feat: cleaned up unused state and added ability to update fighter dispatch

--- src/components/PokemonPickerList.jsx
+++ src/components/PokemonPickerList.jsx
@@ -1,25 +1,19 @@
-import { useState } from 'react'
-
 import { useGlobalPokemonData } from '../context/globalPokemonList'
-
 import { capitalize } from '../utils/helperFunctions'
+import { useGlobalPlayerData } from '../context/globalPlayerData'
 
 
-const PokemonPickerList = () => {
+const PokemonPickerList = ({ playerNum }) => {
 
     // Should pass the list down as prop? 
-    // The point of the global context is that we don't need to and can just use the hook here right?
-    const { pokemonList, setPokemonList } = useGlobalPokemonData()
+    // The point of the global context is that we don't need to and can just use the hook here right? - Matt: yep, we can use the global context here
+    const { pokemonList } = useGlobalPokemonData()
+    const { playerDispatch } = useGlobalPlayerData()
 
-
-    // Not sure how you want to use useReducer here so I'm just storing in a state variable for now and logging it
-    const [selectedPokemonId, setSelectedPokemonId] = useState(null)
-
+    // Need to do [id - 1] because the pokemonList is 0-indexed
     const handleSelect = (id) => {
-        console.log('selected id: ', id)
-        setSelectedPokemonId(id)
+        playerDispatch({ type: 'update', player: playerNum, payload: pokemonList[id - 1] })
     }
-
 
     return (
         <div className='grid grid-cols-4 gap-2 p-2 overflow-y-scroll'>
