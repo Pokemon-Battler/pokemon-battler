f63707ca2353dd98dbe1828715b48972d5cb4c10
socialtonics <mjkgarrow@gmail.com>
2023-05-22 12:24:31 +1000

    feat: swapped png for svg for faster load, about 10x smaller file

--- src/components/LoadingPokeball.jsx
+++ src/components/LoadingPokeball.jsx
@@ -1,7 +1,8 @@
-import Pokeball from '../images/pokeball.png'
+// import Pokeball from '../images/pokeball.png'
+import Pokeball from '../images/pokeball.svg'
 
 const LoadingPokeball = () => {
-    return <div className="h-screen grid place-items-center">
+	return <div className="h-screen grid place-items-center">
 		<img src={Pokeball} alt="pokeball" className='w-24 animate-spin' />
 	</div>
 }
