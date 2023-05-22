2921fe4c32f4b84bf8d19a767240b1c25f3f60da
socialtonics <mjkgarrow@gmail.com>
2023-05-22 12:32:11 +1000

    feat: changed variable names to drop 'fighter', made pokemon data get passed through props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,41 +1,31 @@
-import { useEffect } from 'react'
-
-import { useGlobalPokemonData } from '../context/globalPokemonList'
-
 import { capitalize } from '../utils/helperFunctions'
 
-const PokemonCard = ({ fighterNum, pokemonId }) => {
-    const { pokemonList, setPokemonList } = useGlobalPokemonData()
-
-    const fighter = pokemonList[pokemonId] || null
-    const hp = fighter?.stats.hp
-
+const PokemonCard = ({ playerNum, pokemon }) => {
     return (
         <div className='border rounded bg-purple-500/10'>
-            {fighter && (
+            {pokemon && (
                 <div className='flex flex-col gap-2 px-2'>
                     <div className='flex items-center justify-between'>
-                        <p>Fighter {fighterNum}</p>
+                        <p>Player {playerNum}</p>
                         <p className='text-4xl font-semibold'>
-                            {capitalize(fighter.name)}
+                            {capitalize(pokemon.name)}
                         </p>
                         <div className='space-x-1'>
                             <span className='font-bold text-xs'>HP</span>
-                            <span className='font-semibold text-4xl'>{hp}</span>
+                            <span className='font-semibold text-4xl'>{pokemon.stats.hp}</span>
                         </div>
                     </div>
 
                     <div className=''>
                         <img
-                            src={fighter.sprites.front}
+                            src={pokemon.sprites.highRes}
                             alt='pokemon fighter'
                             className='w-1/2 mx-auto border-2 rounded '
                         />
                     </div>
 
                     <div>
-                        {/* {fighter.abilities[0].name} */}
-                        {fighter?.abilities.map((ability, index) => (
+                        {pokemon.abilities.map((ability, index) => (
                             <div key={index} className='text-center p-4 border'>
                                 {capitalize(ability)}
                             </div>
@@ -43,7 +33,7 @@ const PokemonCard = ({ fighterNum, pokemonId }) => {
                     </div>
 
                     <div className="grid grid-cols-3 grid-rows-2 grid-flow-col gap-2">
-                        {Object.entries(fighter?.stats).slice(1).map(([key, value]) => (
+                        {Object.entries(pokemon.stats).slice(1).map(([key, value]) => (
                             <div key={key} className="p-1 border rounded">
                                 <span>{capitalize(key)}: </span>
                                 <span>{value}</span>
